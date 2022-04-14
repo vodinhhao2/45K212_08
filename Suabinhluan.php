549fd09aa44928721a73bb4e7bbe25fb3c92c9f2
Võ Đình Hảo <99672704+vodinhhao2@users.noreply.github.com>
2022-04-14 22:53:08 +0700

    Delete Suabinhluan.php

--- Suabinhluan.php
+++
@@ -1,9 +0,0 @@
-<?php
-	ob_start();
-	include("../connect.php");
-	if(isset($_GET['binhluan']))
-	{
-		mysql_query("UPDATE binhluan SET id=);
-	}
-	header("location:cn_binhluan".$_GET['Binhluan']);
-?>
